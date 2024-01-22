a41da2a0a73abe475ad42064c2173da75868efff
gwoongchoi <opo1220@gmail.com>
2024-01-23 03:31:17 +0900

    forum first settings

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { connectDB } from "@/util/database";
+import { MongoClient } from "mongodb";
+
+export default async function Home() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
+  return <div>Hello</div>;
+}
